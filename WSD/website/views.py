28569c474eab4ff69c15fe44c9a7a249e42e5ac6
Nourah Alqhtani <nourah0qq@gmail.com>
2022-10-30 18:29:18 +0300

    update password

--- WSD/website/views.py
+++ WSD/website/views.py
@@ -4,15 +4,19 @@ from datetime import date
 import random
 
 # Create your views here.
-Password = ["1111111","2222222","3333333","4444444"]
+#Password = ["1111111","2222222","3333333","4444444"]
 
 def Date_Of_Today(request : HttpRequest):
     Today = {"Date": date.today() }
     return render(request,"templatesweb/date.html", Today)
 
 def Randomly_Password(request: HttpRequest):
-    Random_Password ={"Random_Password" : random.choice(Password)}
-    return render(request, "templatesweb/Password.html", Random_Password)
+    RPL = random.choices("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890@#$", k = 15)
+    pas = "".join(RPL)
+    return render(request, "templatesweb/Password.html", {"Random_Password" : pas})
+
+    #Random_Password ={"Random_Password" : random.choice(Password)}
+    #return render(request, "templatesweb/Password.html", Random_Password)
 
 def Favorite_Games(request: HttpRequest):
     Games = {"Games" : ["Game 1", "Game 2", "Game 3"]}
