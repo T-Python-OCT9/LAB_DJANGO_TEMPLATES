60733a3a2ebfef7ef4fd86d6e74346fdf0833b77
Najd <nani1997.na@gmail.com>
2022-10-31 19:23:40 +0300

    LAB_DJANGO_TEMPLATE_INHERITANCE

--- Najd/website/urls.py
+++ Najd/website/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from .views import todayView, randomPasswordView, favoriteGamesView
+from .views import todayView, random_password, favoriteGamesView
 
 app_name ='website'
 
 urlpatterns =[
     path('today/' ,todayView , name='today'),
-    path('random/password/' , randomPasswordView , name='random_password'),
-    path('favs/games/' , favoriteGamesView, name='favorite_games'),
+    path('random/password/',random_password, name="random_pass"),
+    path('favs/games/' , favoriteGamesView, name='games'),
 
 ]
